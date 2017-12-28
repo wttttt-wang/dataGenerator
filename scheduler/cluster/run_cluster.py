78d4fcf8123cbd6fe4c8ef15dc71c88bde8a269c
wttttt <wttttt@Wttttt-de-MacBookPro.local>
2017-12-28 22:35:10 +0800

    new funcs.

--- scheduler/cluster/run_cluster.py
+++ scheduler/cluster/run_cluster.py
@@ -1,5 +1,6 @@
 import run_gmm
 import run_kmeans
+import run_GaussianMixture
 
 data_path = "/home/testData/cluster1.txt"
 hdfs_path = "/testData/cluster1.txt"
@@ -7,3 +8,4 @@ run_gmm.gen_data(data_path, hdfs_path)
 
 run_gmm.run_jar(hdfs_path)
 run_kmeans.run_jar(hdfs_path)
+run_GaussianMixture.run_jar(hdfs_path)
